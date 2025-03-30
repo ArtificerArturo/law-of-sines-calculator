9a3366cd28d40ad451e24bad02f17958a6ad5fb5
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-30 00:01:45 -0500

    created solve switcher functionality and implemented the formula

--- script.js
+++ script.js
@@ -4,20 +4,127 @@ function calculateSines() {
    const infoElement = document.querySelector("#sinesCalculator .info")
    const sideAInput = document.querySelector("#sinesCalculator #sideAInput")
    const sideBInput = document.querySelector("#sinesCalculator #sideBInput")
+   const alphaInput = document.querySelector("#sinesCalculator #alphaInput")
    const betaInput = document.querySelector("#sinesCalculator #betaInput")
-   //let result = 0 //change to alpha
-   let sideA = sideAInput.value
-   let sideB = sideBInput.value
-   let beta = betaInput.value
+
+   let sideA = sideAInput?.value
+   let sideB = sideBInput?.value
+   let alpha = alphaInput?.value
+   let beta = betaInput?.value
+   console.log(sideA)
+   console.log(sideB)
+   console.log(alpha)
+   console.log(beta)
+
+   infoElement.innerHTML = `<math><mi>a</mi><mo>=</mo><mi>b</mi><mo>&#xd7;</mo><mfrac><mrow><mi>sin</mi><mfenced><mi>&#x3b1;</mi></mfenced></mrow><mrow><mi>sin</mi><mfenced><mi>&#x3b2;</mi></mfenced></mrow></mfrac></math>`
 
    if (dropdown.value == "alpha") {
-      infoElement.innerHTML = `<math><mi>a</mi><mo>=</mo><mi>b</mi><mo>&#xd7;</mo><mfrac><mrow><mi>sin</mi><mfenced><mi>&#x3b1;</mi></mfenced></mrow><mrow><mi>sin</mi><mfenced><mi>&#x3b2;</mi></mfenced></mrow></mfrac></math>`
       alpha = Math.asin(Math.sin(beta) * (sideA / sideB))
       resultElement.innerHTML = `α (radians) = ${resultConditioner(alpha)}`
    } else if (dropdown.value == "sideA") {
-      infoElement.innerHTML = ``
-      resultElement.innerHTML = ``
+      sideA = sideB * (Math.sin(alpha) / Math.sin(beta))
+      resultElement.innerHTML = `Side a = ${resultConditioner(sideA)}`
+   } else if (dropdown.value == "sideB") {
+      sideB = sideA * (Math.sin(beta) / Math.sin(alpha))
+      resultElement.innerHTML = `Side b = ${resultConditioner(sideB)}`
+   } else if (dropdown.value == "beta") {
+      beta = Math.asin(Math.sin(alpha) * (sideB / sideA))
+      resultElement.innerHTML = `β (radians) = ${resultConditioner(beta)}`
    }
+}
+
+function changeFields() {
+   const inputFields = document.querySelector("#sinesCalculator #inputFields")
+   const dropdown = document.querySelector("#sinesCalculator #dropdown")
+   const resultElement = document.querySelector("#sinesCalculator .result")
+
+   let sideALabel = document.createElement("label")
+   sideALabel.setAttribute("for", "sideAInput")
+   sideALabel.textContent = "a side"
+
+   let sideAInput = document.createElement("input")
+   sideAInput.setAttribute("type", "number")
+   sideAInput.setAttribute("placeholder", "Enter value")
+   sideAInput.setAttribute("ID", "sideAInput")
+   sideAInput.setAttribute("onkeyup", "calculateSines()")
+
+   let sideBLabel = document.createElement("label")
+   sideBLabel.setAttribute("for", "sideBInput")
+   sideBLabel.textContent = "b side"
+
+   let sideBInput = document.createElement("input")
+   sideBInput.setAttribute("type", "number")
+   sideBInput.setAttribute("placeholder", "Enter value")
+   sideBInput.setAttribute("ID", "sideBInput")
+   sideBInput.setAttribute("onkeyup", "calculateSines()")
+
+   let alphaLabel = document.createElement("label")
+   alphaLabel.setAttribute("for", "alphaInput")
+   alphaLabel.textContent = "α alpha"
+
+   let alphaInput = document.createElement("input")
+   alphaInput.setAttribute("type", "number")
+   alphaInput.setAttribute("placeholder", "Enter value")
+   alphaInput.setAttribute("ID", "alphaInput")
+   alphaInput.setAttribute("onkeyup", "calculateSines()")
+
+   let betaLabel = document.createElement("label")
+   betaLabel.setAttribute("for", "betaInput")
+   betaLabel.textContent = "β beta"
+
+   let betaInput = document.createElement("input")
+   betaInput.setAttribute("type", "number")
+   betaInput.setAttribute("placeholder", "Enter value")
+   betaInput.setAttribute("ID", "betaInput")
+   betaInput.setAttribute("onkeyup", "calculateSines()")
+
+   let labelDiv = document.createElement("div")
+   labelDiv.setAttribute("ID", "labelDiv")
+   let fieldDiv = document.createElement("div")
+   fieldDiv.setAttribute("ID", "fieldDiv")
+
+   if (dropdown.value == "alpha") {
+      inputFields.replaceChildren()
+      labelDiv.appendChild(sideALabel)
+      labelDiv.appendChild(sideBLabel)
+      labelDiv.appendChild(betaLabel)
+      inputFields.appendChild(labelDiv)
+      fieldDiv.appendChild(sideAInput)
+      fieldDiv.appendChild(sideBInput)
+      fieldDiv.appendChild(betaInput)
+      inputFields.appendChild(fieldDiv)
+   } else if (dropdown.value == "sideA") {
+      inputFields.replaceChildren()
+      labelDiv.appendChild(alphaLabel)
+      labelDiv.appendChild(sideBLabel)
+      labelDiv.appendChild(betaLabel)
+      inputFields.appendChild(labelDiv)
+      fieldDiv.appendChild(alphaInput)
+      fieldDiv.appendChild(sideBInput)
+      fieldDiv.appendChild(betaInput)
+      inputFields.appendChild(fieldDiv)
+   } else if (dropdown.value == "sideB") {
+      inputFields.replaceChildren()
+      labelDiv.appendChild(sideALabel)
+      labelDiv.appendChild(alphaLabel)
+      labelDiv.appendChild(betaLabel)
+      inputFields.appendChild(labelDiv)
+      fieldDiv.appendChild(sideAInput)
+      fieldDiv.appendChild(alphaInput)
+      fieldDiv.appendChild(betaInput)
+      inputFields.appendChild(fieldDiv)
+   } else if (dropdown.value == "beta") {
+      inputFields.replaceChildren()
+      labelDiv.appendChild(sideALabel)
+      labelDiv.appendChild(alphaLabel)
+      labelDiv.appendChild(sideBLabel)
+      inputFields.appendChild(labelDiv)
+      fieldDiv.appendChild(sideAInput)
+      fieldDiv.appendChild(alphaInput)
+      fieldDiv.appendChild(sideBInput)
+      inputFields.appendChild(fieldDiv)
+   }
+   resultElement.innerHTML = ""
 }
 
 function resultConditioner(result) {
